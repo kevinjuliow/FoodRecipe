ec5b5d2f92c0755bd1e6730366b7982ed4c506ce
kevinjuliow <kevjuliow@gmail.com>
2023-05-06 18:49:40 +0700

    unfinished

--- app.js
+++ app.js
@@ -11,20 +11,34 @@ const RecipeLists = [{
   directions : 'Toast waffles according to package directions. Spread each waffle with 2 tablespoons almond butter. Cut waffles into bite-sized pieces. , Layer half the yogurt, bananas, strawberries and waffle pieces into 4 parfait glasses. Repeat layers. If desired, top with toasted almonds and maple syrup. Serve immediately.'
   ,type : 'breakfast' 
   , img : 'Assets/Recipe2.jpg'
-} ]
+} ,{
+  name : 'Spanish Omelet',
+  ingredients : '6 large eggs , 1/4 cup water , 1 cup refried beans, warmed , 1/4 cup chopped red onion , 1/2 cup shredded Mexican cheese blend, divided , 1/4 cup salsa'
+  , type :'breakfast',
+  img : 'Assets/Recipe3.jpg'
+}]
 
 
 const recipeList = document.querySelector('.recipeList-container')
+const breakfast = document.querySelector('.breakfast')
+const lunch = document.querySelector('.lunch')
+const dinner = document.querySelector('.dinner')
 
 
 
 RecipeLists.map((e)=>{
-  const newDiv = document.createElement('div')
-  const img = document.createElement('img')
-  img.setAttribute('src' , e.img)
-  const newP = document.createElement('p')
-  newP.innerHTML = e.name
-  newDiv.append(img)
-  newDiv.append(newP)
-  recipeList.append(newDiv)
+  if (e.type === 'breakfast'){
+    breakfast.style.borderBottom = '1px solid black'
+    const newDiv = document.createElement('div')
+    const img = document.createElement('img')
+    img.setAttribute('src' , e.img)
+    const newP = document.createElement('p')
+    newP.innerHTML = e.name
+    img.style.width = '100%'
+    img.style.borderRadius = '10px'
+    newP.style.textAlign = 'center'
+    newDiv.append(img)
+    newDiv.append(newP)
+    recipeList.append(newDiv)
+  }
 })
